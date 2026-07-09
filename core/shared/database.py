9d87780e7fa1a6b1f24d3bf22a37a966e6812611
Ignaxd1000 <nachoursobaez@gmail.com>
2026-07-09 11:29:33 -0300

    meto todo en database.py para hacer todo mas facilin

--- core/shared/database.py
+++ core/shared/database.py
@@ -19,7 +19,6 @@ class database:
 
             sha256 TEXT,
             isDeleted INTEGER DEFAULT 0,
-            sessionId INTEGER,
 
         )
         """)
@@ -30,3 +29,56 @@ class database:
         self.conn.close()
         return("Database session succesfully closed:")
         
+    def saveFileToDb(self, entry: fileEntry):
+
+        self.cursor.execute("""
+        INSERT INTO files(
+            filePath,
+            extension,
+            size,
+            createdAt,
+            modifiedAt,
+            sha256
+        )
+        VALUES (?, ?, ?, ?, ?, ?)
+        """, (
+            str(entry.filePath),
+            entry.extension,
+            entry.size,
+            entry.createdAt.isoformat(),
+            entry.modifiedAt.isoformat(),
+            entry.sha256
+        ))
+
+        self.conn.commit()
+
+    def getAllFiles(self) -> list[FileEntry]:
+
+        self.cursor.execute("""
+            SELECT
+                filePath,
+                extension,
+                size,
+                createdAt,
+                modifiedAt,
+                sha256
+            FROM files
+        """)
+
+        rows = self.cursor.fetchall()
+
+        files = []
+
+        for row in rows:
+            files.append(
+                FileEntry(
+                    filePath=Path(row[0]),
+                    extension=row[1],
+                    size=row[2],
+                    createdAt=datetime.fromisoformat(row[3]),
+                    modifiedAt=datetime.fromisoformat(row[4]),
+                    sha256=row[5]
+                )
+            )
+
+        return files
