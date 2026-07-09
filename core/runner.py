58aae07818c390a69b5f43d9aab9b3b068482bf8
Ignaxd1000 <nachoursobaez@gmail.com>
2026-07-09 15:28:51 -0300

    sync

--- core/runner.py
+++ core/runner.py
@@ -1,1 +1,9 @@
 from shared.database import database
+from shared.config import Config
+
+
+class runner:
+
+    def __init__(self):
+        self.config = Config()
+        self.db = database(self.config.cfg.dbPath)
